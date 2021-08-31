018ae915f455605036032ec693ca4c6cbc3f2ed1
Maikel Martinez <maikel@wemindinstitute.com>
2021-08-31 13:33:19 -0400

    init

---
+++ index.js
@@ -0,0 +1,105 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Country {
+    country: String
+    year: Int
+    areaSquareKilometres: Int
+    totalPopulation: Int
+  }
+  type Query {
+    countries: [Country]
+  }
+  type Query {
+    country: Country
+  }
+`;
+
+const countries = [
+
+    {
+   
+      country:
+   "Albania",
+   
+      year:
+   2000,
+   
+      areaSquareKilometres:
+   28748,
+   
+      totalPopulation:
+   3401198
+   
+    },
+   
+    {
+   
+      country:
+   "Albania",
+   
+      year:
+   2001,
+   
+      areaSquareKilometres:
+   28748,
+   
+      totalPopulation:
+   3073734
+   
+    },
+   
+    {
+   
+      country:
+   "Albania",
+   
+      year:
+   2002,
+   
+      areaSquareKilometres:
+   28748,
+   
+      totalPopulation:
+   3093465
+   
+    },
+   
+    {
+   
+      Country:
+   "Albania",
+   
+      Year:
+   2003,
+   
+     areaSquareKilometres:
+   28748,
+   
+      totalPopulation:
+   3111162
+   
+    }
+   
+   ]
+
+  // Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+    Query: {
+      countries: () => countries,
+    },
+    Query: {
+      country: () => country,
+    },
+  };
+
+  // The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
+
